b8203b2cacf6f82f378814d12ea4764ccc466e9b
Muhammad Junaid <rjunaid7814@gmail.com>
2026-03-07 08:49:00 +0500

    Uudate Streamlit app code

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import joblib
 
-model = joblib.load('xgb_model.jb')
+model = joblib.load("xgb_model.jb")
 
 st.title("House Price Prediction App")
 st.write("Enter the Details Below to predict the House Price")
@@ -13,20 +13,19 @@ inputs=['OverallQual', 'GrLivArea', 'GarageArea', '1stFlrSF',
        'CentralAir']
 
 input_data = {}
-for features in inputs:
+for features in input:
     if features == 'CentralAir':
-        input[features] = st.selectbox(f"{features}",options=['Yes','No'], index=0)
+        input_data[features] = st.selectbox(f"{features}", options= ['Yes','No'], index=0)
     else:
         input_data[features] = st.number_input(
             f"{features}",
             value=0.0,
-            step=1.0 if features in ['OverallQual','FullBath','Fireplaces'] else 0.1
-        )
+         )
 
 if st.button("Predict Price"):
-    input_data['CentralAir'] =1 if input_data['CentralAir'] == "Yes" else 0.1
+    input_data['CentralAir'] = 1 if input_data['CentralAir'] == "Yes" else 0
 
-    input_df = pd.DataFrame([input_data],columns=inputs)
+    input_df = pd.DataFrame([input_data], columns=inputs)
 
-    predictions = model.predict(input_df)
-    st.success(f"Predicted House Price: ${predictions[0]:,.2f}")
+    prediction = model.predict(input_df)
+    st.success(f"Predicted House Price: ${prediction[0]:,.2f}")
